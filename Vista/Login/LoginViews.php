01cdba1fcbb429ae3e1cd6c5d3ec62e21a8ebb7a
jonandervicente <2gdaw07@a224p07.arriagainfo.local>
2017-02-09 09:08:40 +0100

    Login4
    Cambio de login3 a 4

--- Vista/Login/LoginViews.php
+++ Vista/Login/LoginViews.php
@@ -20,7 +20,11 @@ class LoginViews extends Views
         ?>
                 <form name="loginForm" class="form-horizontal login" method="post">
                     <fieldset>
+<<<<<<< Updated upstream
                         <legend>Login3</legend>
+=======
+                        <legend>Login4</legend>
+>>>>>>> Stashed changes
                     <div class="form-group">
                         <label class="control-label col-sm-2 col-sm-offset-2 col-md-2 col-md-offset-2">DNI:</label>
                         <div class="col-sm-4 col-md-4">
